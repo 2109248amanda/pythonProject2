9d50da16209d521d91b09b89d4c91ad4f950b330
Tabata9 <tabatarsouza@gmail.com>
2023-05-11 21:02:18 -0300

    projeto raiz

---
+++ app.py
@@ -0,0 +1,43 @@
+from urllib import request
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/quem-somos')
+def quem_somos():
+    return render_template('quem_somos.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/quero-doar')
+def quero_doar():
+    return render_template('quero_doar.html')
+
+@app.route('/quero-receber')
+def quero_receber():
+    return render_template('quero_receber.html')
+
+    roupas_infantil = request.form.get('roupa_infantil')
+    roupas_adulto = request.form.get('roupa_adulto')
+    alimentos = request.form.get('alimentos')
+    moveis = request.form.get('moveis')
+    medicamentos = request.form.get('medicamentos')
+    produtos_gerais = request.form.get('produtos_gerais')
+
+    # Faça o processamento necessário para cada item selecionado
+
+    return render_template('quero-receber.html')
+
+@app.route('/nossos-contatos')
+def nossos_contatos():
+    return render_template('nossos_contatos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
